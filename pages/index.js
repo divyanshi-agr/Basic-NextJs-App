c49333f53d09538a4ed7ac1f1d7360c6f29c0f54
Divyanshi Agrawal <divsagr3@gmail.com>
2021-05-09 22:58:44 +0530

    Another change

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Hello Next</div>;
+  return <div className={styles.container}>Hello Next App</div>;
 }
